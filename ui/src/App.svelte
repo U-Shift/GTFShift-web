c371862b4f7102fb8c6d267eb4b98e1fb646540e
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-01-22 17:05:50 +0000

    ui base map kick-off

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,47 +1,49 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import * as L from 'leaflet';
+  // If you're playing with this in the Svelte REPL, import the CSS using the
+  // syntax in svelte:head instead. For normal development, this is better.
+  import 'leaflet/dist/leaflet.css';
+  let map;
+
+  function createMap(container) {
+    let m = L.map(container, {
+      zoomControl: false
+    }).setView([38.7169, -9.1399], 3);
+
+    L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
+      {
+        attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+          &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+        subdomains: 'abcd',
+        maxZoom: 14
+      }
+    ).addTo(m);
+
+    L.control.zoom({
+        position: 'bottomright'
+    }).addTo(m);
+
+    return m;
+  }
+
+  function mapAction(container) {
+    map = createMap(container);
+    return {
+      destroy: () => {
+        map.remove();
+      },
+    };
+  }
 </script>
 
+<svelte:head>
+   <!-- In the REPL you need to do this. In a normal Svelte app, use a CSS Rollup plugin and import it from the leaflet package. -->
+   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+</svelte:head>
+
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div style="height:100vh;width:100vw" use:mapAction></div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
