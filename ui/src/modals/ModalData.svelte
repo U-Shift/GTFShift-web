d4c1cad10af00236617fba653ed6f622ec001126
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-02-02 16:45:25 +0000

    dashboard attributes table modal dynamic update fix

--- ui/src/modals/ModalData.svelte
+++ ui/src/modals/ModalData.svelte
@@ -17,7 +17,17 @@
     } = $props();
 
     let data_filtered = $state<Feature<any>[]>([]);
-    let table = $state<TableHandler<any> | undefined>(undefined);
+    let table = $state<TableHandler<any>>(
+        new TableHandler(data_filtered, {
+            rowsPerPage: 10,
+            i18n: {
+                // Characters
+                previous: "<",
+                next: ">",
+                rowCount: "{start} to {end}, of {total} entries",
+            },
+        }),
+    );
 
     $effect(() => {
         if (geoData) {
@@ -33,21 +43,7 @@
         }
 
         untrack(() => {
-            // Reset table when data changes
-            table = new TableHandler(data_filtered, {
-                rowsPerPage: 10,
-                i18n: {
-                    // Characters
-                    previous: "<",
-                    next: ">",
-                    rowCount: "{start} to {end}, of {total} entries",
-                },
-            });
-            table.clearFilters();
-            table.clearSearch();
-            table.clearSort();
-            table.clearSelection();
-            table.setPage(1);
+            table.setRows([...data_filtered]);
         });
     });
 </script>
@@ -63,68 +59,74 @@
                 </h4>
             </header>
             {#if table}
-            <Datatable basic {table}>
-                <table>
-                    <thead>
-                        <tr>
-                            <ThSort {table} field="way_osm_id">OSM ID</ThSort>
-                            <ThSort {table} field="frequency">Frequency</ThSort>
-                            <ThSort {table} field="is_bus_lane">Bus Lane</ThSort
-                            >
-                            <ThSort {table} field="n_lanes">Nr lanes</ThSort>
-                            <ThSort {table} field="n_directions"
-                                >Nr directions</ThSort
-                            >
-                            <ThSort {table} field="n_lanes_direction"
-                                >Nr lanes/dir</ThSort
-                            >
-                            {#if rt_data}
-                                <ThSort {table} field="speed_avg"
-                                    >Avg speed</ThSort
-                                >
-                                <ThSort {table} field="speed_p25"
-                                    >Speed P25</ThSort
-                                >
-                                <ThSort {table} field="speed_median"
-                                    >Speed Median</ThSort
-                                >
-                                <ThSort {table} field="speed_p75"
-                                    >Speed P75</ThSort
-                                >
-                                <ThSort {table} field="speed_count"
-                                    >Speed Count</ThSort
-                                >
-                            {/if}
-                            <ThSort {table} field="routes">Routes</ThSort>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#each table.rows as row}
+                <Datatable basic {table}>
+                    <table>
+                        <thead>
                             <tr>
-                                <td>
-                                    <a
-                                        href="https://www.openstreetmap.org/way/{row.way_osm_id}"
-                                        target="_blank">{row.way_osm_id}</a
-                                    >
-                                </td>
-                                <td>{row.frequency}</td>
-                                <td>{row.is_bus_lane}</td>
-                                <td>{row.n_lanes}</td>
-                                <td>{row.n_directions}</td>
-                                <td>{row.n_lanes_direction}</td>
+                                <ThSort {table} field="way_osm_id"
+                                    >OSM ID</ThSort
+                                >
+                                <ThSort {table} field="frequency"
+                                    >Frequency</ThSort
+                                >
+                                <ThSort {table} field="is_bus_lane"
+                                    >Bus Lane</ThSort
+                                >
+                                <ThSort {table} field="n_lanes">Nr lanes</ThSort
+                                >
+                                <ThSort {table} field="n_directions"
+                                    >Nr directions</ThSort
+                                >
+                                <ThSort {table} field="n_lanes_direction"
+                                    >Nr lanes/dir</ThSort
+                                >
                                 {#if rt_data}
-                                    <td>{row.speed_avg?.toFixed(2)}</td>
-                                    <td>{row.speed_p25?.toFixed(2)}</td>
-                                    <td>{row.speed_median?.toFixed(2)}</td>
-                                    <td>{row.speed_p75?.toFixed(2)}</td>
-                                    <td>{row.speed_count}</td>
+                                    <ThSort {table} field="speed_avg"
+                                        >Avg speed</ThSort
+                                    >
+                                    <ThSort {table} field="speed_p25"
+                                        >Speed P25</ThSort
+                                    >
+                                    <ThSort {table} field="speed_median"
+                                        >Speed Median</ThSort
+                                    >
+                                    <ThSort {table} field="speed_p75"
+                                        >Speed P75</ThSort
+                                    >
+                                    <ThSort {table} field="speed_count"
+                                        >Speed Count</ThSort
+                                    >
                                 {/if}
-                                <td>{row.routes}</td>
+                                <ThSort {table} field="routes">Routes</ThSort>
                             </tr>
-                        {/each}
-                    </tbody>
-                </table>
-            </Datatable>
+                        </thead>
+                        <tbody>
+                            {#each table.rows as row}
+                                <tr>
+                                    <td>
+                                        <a
+                                            href="https://www.openstreetmap.org/way/{row.way_osm_id}"
+                                            target="_blank">{row.way_osm_id}</a
+                                        >
+                                    </td>
+                                    <td>{row.frequency}</td>
+                                    <td>{row.is_bus_lane}</td>
+                                    <td>{row.n_lanes}</td>
+                                    <td>{row.n_directions}</td>
+                                    <td>{row.n_lanes_direction}</td>
+                                    {#if rt_data}
+                                        <td>{row.speed_avg?.toFixed(2)}</td>
+                                        <td>{row.speed_p25?.toFixed(2)}</td>
+                                        <td>{row.speed_median?.toFixed(2)}</td>
+                                        <td>{row.speed_p75?.toFixed(2)}</td>
+                                        <td>{row.speed_count}</td>
+                                    {/if}
+                                    <td>{row.routes}</td>
+                                </tr>
+                            {/each}
+                        </tbody>
+                    </table>
+                </Datatable>
             {/if}
 
             <footer>
