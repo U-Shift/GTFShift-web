653c353df1b13ddf615f94ab9e920cfec62d273c
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-05-08 14:33:14 +0100

    #8 script to generate GTFS with OSM shapes generating zip

--- scripts/01_osm_match/osm_match_create_gtfs.R
+++ scripts/01_osm_match/osm_match_create_gtfs.R
@@ -14,7 +14,7 @@ region <- regions |> slice_head(n = 1)
 # Load GTFS
 gtfs_file <- sprintf("%s/gtfs_%s_%s.zip", output_root, region$name, region$gtfs_day)
 gtfs <- GTFShift::load_feed(region$gtfs_url, create_transfers = FALSE)
-gtfs_shapes <- tidytransit::shapes_as_sf(gtfs$shapes)
+shapes_gtfs <- tidytransit::shapes_as_sf(gtfs$shapes)
 summary(gtfs)
 
 # Load OSM shapes
@@ -32,28 +32,46 @@ summary(gtfs_filtered)
 
 # Replace shapes on GTFS
 gtfs_filtered$shapes
-shapes_test_multi <- osm_shapes |>
+shapes_osm_multi <- osm_shapes |>
     st_transform(4326) |>
     st_cast("MULTILINESTRING")
-nrow(shapes_test_multi)
+nrow(shapes_osm_multi)
 
-coords <- st_coordinates(shapes_test_multi)
+coords <- st_coordinates(shapes_osm_multi)
 length(coords)
 
-shapes_test <- data.frame(
-    shape_id = shapes_test_multi$shape_id[coords[, "L2"]],
+shapes_osm <- data.frame(
+    shape_id = shapes_osm_multi$shape_id[coords[, "L2"]],
     shape_pt_lat = coords[, "Y"],
     shape_pt_lon = coords[, "X"],
     stringsAsFactors = FALSE
 ) |>
     group_by(shape_id) |>
     mutate(shape_pt_sequence = row_number()) |>
+    arrange(shape_id, shape_pt_sequence) |>
     ungroup()
-shapes_test_sf_points <- shapes_test |> st_as_sf(coords = c("shape_pt_lon", "shape_pt_lat"), crs = 4326)
-shapes_test_sf_routes <- tidytransit::shapes_as_sf(shapes_test)
 
-mapview(gtfs_shapes |> filter(shape_id == "143_1_ASC_shp"), layer.name = "Original Shape", color = "gray") +
-    mapview(shapes_test_sf_routes |> filter(shape_id == "143_1_ASC_shp"), layer.name = "OSM Shape", color = "black") +
-    mapview(shapes_test_sf_points |> filter(shape_id == "143_1_ASC_shp"), layer.name = "OSM Points", color = "black", point_size = 0.5, zcol = "shape_pt_sequence")
+gtfs_filtered$shapes <- shapes_osm
 
-gtfs_filtered$shapes <- shapes_test
+# Append _osm to gtfs_file
+gtfs_file_osm <- sub(".zip", "_osm.zip", gtfs_file)
+tidytransit::write_gtfs(gtfs_filtered, gtfs_file_osm)
+
+# Debug
+shapes_osm_sf_points <- shapes_osm |> st_as_sf(coords = c("shape_pt_lon", "shape_pt_lat"), crs = 4326)
+shapes_osm_sf_routes <- shapes_osm_sf_points |>
+    group_by(shape_id) |>
+    summarize(geometry = st_union(geometry)) |>
+    st_cast("LINESTRING")
+shapes_osm_sf_routes <- tidytransit::shapes_as_sf(shapes_osm)
+
+mapview(shapes_gtfs |> filter(shape_id == "143_1_ASC_shp"), layer.name = "Original Shape", color = "gray") +
+    mapview(shapes_osm_sf_routes |> filter(shape_id == "143_1_ASC_shp"), layer.name = "OSM Shape", color = "black") +
+    mapview(
+        shapes_osm_sf_points |> filter(shape_id == "143_1_ASC_shp"),
+        layer.name = "OSM Points", color = "black", point_size = 0.5, zcol = "shape_pt_sequence"
+    )
+
+osm_shapes |> filter(shape_id == "143_1_ASC_shp")
+
+gtfs_filtered$shapes <- shapes_osm
