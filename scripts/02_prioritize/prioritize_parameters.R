142c1c9b4989694d6a0c3622026d55f0bed3aa1a
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-07-02 11:57:22 +0100

    #18 #20 prioritize parameters for new speeds and demand data

--- scripts/02_prioritize/prioritize_parameters.R
+++ scripts/02_prioritize/prioritize_parameters.R
@@ -1,8 +1,9 @@
-# Parameters
+# Initialization -------------------------------------------------------
 output <- "web_data"
 stop_buffer_size <- 15 # meters
 GTFShiftVersion <- "0.10 (dev version)" # as.character(packageVersion("GTFShift"))
 
+# Define regions to analyse
 regions <- data.frame(
   name = character(),
   name_long = character(),
@@ -15,13 +16,15 @@ regions <- data.frame(
 data <- read.csv(system.file("extdata", "gtfs_sources_pt.csv", package = "GTFShift"))
 
 
-regions <- bind_rows( # Lisboa
+# Lisbon Metro Area -------------------------------------------------------
+## Carris, Lisboa -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "lisboa_rt",
     name_long = "Lisboa, Portugal",
     gtfs_url = "web_data/lisboa_rt/gtfs_20260520/run_20260520_082954/gtfs_lisboa_rt_2026-05-20.zip", # data[data$ID == "lisboa", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = "2026-05-20", # as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_lx",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -30,29 +33,30 @@ regions <- bind_rows( # Lisboa
     metric_crs = 3763,
     rt_interval = "13-30/04/2026 (Business Days)",
     rt_collection = I(list(as.character(list.files(
-      "data/cm_20260413_220260430_business",
+      "data/cm_20260413_220260430_business/processing_after_duplicate_timestamp_fix_withFirstAndLastStops",
       pattern = "^updates_with_speed_.*\\.csv$",
       full.names = TRUE
     )))),
     rt_collection_manipulate = I(list(function(df) {
       df |>
         mutate(
-          lon = stringr::str_replace(lon, "c\\(", ""),
-          lat = stringr::str_replace(lat, "\\)", ""),
-          speed = as.numeric(speed)
+          speed = as.numeric(euclidean_speed_kmh)
         ) |>
-        st_as_sf(coords = c("lon", "lat"), crs = 4326)
+        st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
     })),
+    rt_notes = "Due to the fact that Carris GTFS-RT does not disclose vehicles' momentary speed, a proxy was computed for each TripUpdate considering the time and euclidean distance between each update and the previous one. Refer to the <a href=\"https://github.com/U-Shift/GTFShift-web/blob/main/scripts/00_fetch_rt/gtfs_rt_gather_CarrisMunicipal.Rmd\" target=\"_blank\">script</a> for more details.",
     geofabrik_region = "europe/portugal"
   )
 )
-regions <- bind_rows( # CarrisMetropolitana
+
+## Carris Metropolitana -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "aml_rt",
     name_long = "Lisboa Metro Area, Portugal",
     gtfs_url = data[data$ID == "AML", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_met",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -70,13 +74,15 @@ regions <- bind_rows( # CarrisMetropolitana
     geofabrik_region = "europe/portugal"
   )
 )
-regions <- bind_rows( # CarrisMetropolitana, Area 1
+
+### Area 1 -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "aml_rt_area_1",
     name_long = "Lisboa Metro Area, Portugal (Area 1)",
     gtfs_url = data[data$ID == "AML", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_met_area_1",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -95,13 +101,14 @@ regions <- bind_rows( # CarrisMetropolitana, Area 1
   )
 )
 
-regions <- bind_rows( # CarrisMetropolitana, Area 2
+### Area 2 -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "aml_rt_area_2",
     name_long = "Lisboa Metro Area, Portugal (Area 2)",
     gtfs_url = data[data$ID == "AML", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_met_area_2",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -120,38 +127,56 @@ regions <- bind_rows( # CarrisMetropolitana, Area 2
   )
 )
 
-regions <- bind_rows( # CarrisMetropolitana, Area 3
+### Area 3 -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "aml_rt_area_3",
     name_long = "Lisboa Metro Area, Portugal (Area 3)",
-    gtfs_url = data[data$ID == "AML", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_url = "web_data/aml_rt_area_3/gtfs_20260520/run_20260518_203125/gtfs_aml_rt_area_3_2026-05-20.zip", # data[data$ID == "AML", ]$URL,
+    gtfs_day = "2026-05-20", # as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_met_area_3",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "network", value = "Carris Metropolitana", key_exact = TRUE)
     ))),
     rt_interval = "13-30/04/2026 (Business Days)",
-    rt_collection = I(list("data/cmet_20260413_220260430_business_a3/updates.csv")),
+    # rt_collection = I(list("data/cmet_20260413_220260430_business_a3/updates_instantSpeeds.csv")),
+    # rt_collection_manipulate = I(list(function(df) {
+    #  df |>
+    #    filter(current_status == "IN_TRANSIT_TO") |>
+    #    mutate(
+    #      speed = as.numeric(speed)
+    #    ) |>
+    #    st_as_sf(coords = c("lon", "lat"), crs = 4326)
+    # })),
+    rt_collection = I(list(as.character(list.files(
+      "data/cmet_20260413_220260430_business_a3",
+      pattern = "^updates_with_speed_.*\\.csv$",
+      full.names = TRUE
+    )))),
     rt_collection_manipulate = I(list(function(df) {
       df |>
         mutate(
-          speed = as.numeric(speed)
+          speed = as.numeric(euclidean_speed_kmh)
         ) |>
-        st_as_sf(coords = c("lon", "lat"), crs = 4326)
+        st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
     })),
-    geofabrik_region = "europe/portugal"
+    demand_for_route = I(list("data/demand/cm_demand_20260520.csv")),
+    demand_notes = "Demand by route, for same representative day as transit services analysed. Segment aggregation considers the sum of all passengers that board each route that passes through the segment for that day, regardless of the direction of travel or stop they boarded or alighted.",
+    geofabrik_region = "europe/portugal",
+    metric_crs = 3763
   )
 )
 
-regions <- bind_rows( # CarrisMetropolitana, Area 4
+### Area 4 -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "aml_rt_area_4",
     name_long = "Lisboa Metro Area, Portugal (Area 4)",
     gtfs_url = data[data$ID == "AML", ]$URL,
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_carris_met_area_4",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -170,14 +195,14 @@ regions <- bind_rows( # CarrisMetropolitana, Area 4
   )
 )
 
-
-regions <- bind_rows( # Cascais
+## MobiCascais, Cascais -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "cascais",
     name_long = "Cascais, Portugal",
     gtfs_url = "https://drive.google.com/uc?export=download&id=13ucYiAJRtu-gXsLa02qKJrGOgDjbnUWX",
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "network", value = "MobiCascais", key_exact = TRUE)
@@ -186,65 +211,84 @@ regions <- bind_rows( # Cascais
   )
 )
 
-
-regions <- bind_rows( # Barreiro
+## TCB, Barreiro -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "barreiro",
     name_long = "Barreiro, Portugal",
-    gtfs_url = data$URL[data$ID == "barreiro"],
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    # gtfs_url = data$URL[data$ID == "barreiro"],
+    gtfs_url = "web_data/barreiro/gtfs_20260520/run_20260519_110700/gtfs_barreiro_2026-05-20.zip",
+    # gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
+    gtfs_day = "2026-05-20",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
-      list(key = "network", value = c("TCB", "Transportes Coletivos do Barreiro"), key_exact = TRUE)
+      list(key = "network", value = c("TCB", "Transportes Coletivos do Barreiro", "Transportes Colectivos do Barreiro"), key_exact = TRUE)
     ))),
     rt_interval = "11-15/05/2026",
-    rt_collection = I(list("data/barreiro_20260511_20260515/updates.csv")),
+    rt_collection = I(list(as.character(list.files(
+      "data/barreiro_20260511_20260515",
+      pattern = "^updates_with_speed_.*\\.csv$",
+      full.names = TRUE
+    )))),
     rt_collection_manipulate = I(list(function(df) {
       df |>
         mutate(
-          speed = as.numeric(speed)
+          speed = as.numeric(euclidean_speed_kmh)
         ) |>
-        st_as_sf(coords = c("lon", "lat"), crs = 4326)
+        st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
     })),
-    geofabrik_region = "europe/portugal"
+    rt_notes = "Due to the fact that TCB GTFS-RT does not disclose vehicles' momentary speed, a proxy was computed for each TripUpdate considering the time and euclidean distance between each update and the previous one. Refer to the <a href=\"https://github.com/U-Shift/GTFShift-web/blob/main/scripts/00_fetch_rt/gtfs_rt_gather_TCBarreiro.Rmd\" target=\"_blank\">script</a> for more details.",
+    geofabrik_region = "europe/portugal",
+    metric_crs = 3763
   )
 )
 
-regions <- bind_rows( # STCP
+# Portugal, Others -----------------------------------
+## STCP, Porto -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "stcp",
     name_long = "Porto, Portugal",
-    gtfs_url = "https://api.stcp.pt:8443/v1/ficheiros/estatico/ficheirozip",
-    gtfs_url_headers = I(list(list(
-      "X-App-Id" = Sys.getenv("GTFS_STCP_KEY"),
-      "X-Api-Key" = Sys.getenv("GTFS_STCP_SECRET")
-    ))),
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    # gtfs_url = "https://api.stcp.pt:8443/v1/ficheiros/estatico/ficheirozip",
+    # gtfs_url_headers = I(list(list(
+    #  "X-App-Id" = Sys.getenv("GTFS_STCP_KEY"),
+    #  "X-Api-Key" = Sys.getenv("GTFS_STCP_SECRET")
+    # ))),
+    # gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
+    gtfs_url = "web_data/stcp/gtfs_20260520/run_20260519_073433/gtfs_stcp_2026-05-20.zip",
+    gtfs_day = "2026-05-20",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "operator", value = "STCP", key_exact = TRUE)
     ))),
     rt_interval = "13-30/04/2026 (Business Days)",
-    rt_collection = I(list("data/stcp_20260413_220260430_business/updates.csv")),
+    rt_collection = I(list(as.character(list.files(
+      "data/stcp_20260413_220260430_business",
+      pattern = "^updates_with_speed_.*\\.csv$",
+      full.names = TRUE
+    )))),
     rt_collection_manipulate = I(list(function(df) {
       df |>
         mutate(
-          speed = as.numeric(speed)
+          speed = as.numeric(euclidean_speed_kmh)
         ) |>
-        st_as_sf(coords = c("lon", "lat"), crs = 4326)
+        st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
     })),
-    geofabrik_region = "europe/portugal"
+    geofabrik_region = "europe/portugal",
+    metric_crs = 3763
   )
 )
 
-regions <- bind_rows( # NYC, MTA
+# International -----------------------------------
+## MTA, NYC, USA -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "nyc_mta",
     gtfs_url = "https://rrgtfsfeeds.s3.amazonaws.com/gtfs_busco.zip",
-    gtfs_day = Sys.Date(),
+    gtfs_day = as.character(Sys.Date()),
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "operator", value = "Metropolitan Transportation Authority", key_exact = TRUE)
@@ -252,7 +296,7 @@ regions <- bind_rows( # NYC, MTA
   )
 )
 
-# Ghelph, CA
+## Guelph, CANADA -----------------------------------
 regions <- bind_rows(
   regions,
   data.frame(
@@ -268,13 +312,14 @@ regions <- bind_rows(
   )
 )
 
-regions <- bind_rows( # Madrid
+## EMT, Madrid, SPAIN -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "madrid",
     name_long = "Madrid, ES",
     gtfs_url = "https://servicios.emtmadrid.es:8443/gtfs/transitemt.zip",
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "operator", value = "Empresa Municipal de Transportes de Madrid", key_exact = TRUE)
@@ -283,13 +328,14 @@ regions <- bind_rows( # Madrid
   )
 )
 
-regions <- bind_rows( # Fuenlabrada, ES
+## Fuenlabrada, SPAIN -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "fuenlabrada",
     name_long = "Fuenlabrada, ES",
     gtfs_url = "https://api.control.optibus.co/opendata/v1/gtfs?uid=c-5cfcd2d1",
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     gtfs_manipulate = "manipulate_gtfs_fuenlabrada",
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
@@ -299,13 +345,14 @@ regions <- bind_rows( # Fuenlabrada, ES
   )
 )
 
-regions <- bind_rows( # Toulouse, FR
+## Tisseo, Toulouse, FRANCE -----------------------------------
+regions <- bind_rows(
   regions,
   data.frame(
     name = "toulouse",
     name_long = "Toulouse, FR",
     gtfs_url = "https://data.toulouse-metropole.fr/explore/dataset/tisseo-gtfs/files/fc1dda89077cf37e4f7521760e0ef4e9/download/",
-    gtfs_day = GTFShift::calendar_nextBusinessWednesday(),
+    gtfs_day = as.character(GTFShift::calendar_nextBusinessWednesday()),
     query = I(list(list(
       list(key = "route", value = c("bus"), key_exact = TRUE),
       list(key = "network", value = "Tisséo", key_exact = TRUE)
@@ -314,7 +361,7 @@ regions <- bind_rows( # Toulouse, FR
   )
 )
 
-# Helpers
+# Helpers -----------------------------------
 
 manipulate_carris_met <- function(gtfs) {
   # Remove all text from [ to ] from gtfs$shape_ids, which are present in Carris Metropolitana feed and cause issues in matching with OSM shapes
