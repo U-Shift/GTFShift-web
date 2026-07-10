7b94c47848223d7b4b7108598ddfed71c2bd8abf
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-07-10 11:53:26 +0100

    #20 url for stage

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/~ushift.daemon/apps/gtfshift/dev/',
+  base: '/~ushift.daemon/apps/gtfshift/stage/',
   plugins: [svelte(), tailwindcss()],
   resolve: {
     alias: {
