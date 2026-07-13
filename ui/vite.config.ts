d2b87cf4862ec2939a8a79dbe5742f248cc54a03
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-07-13 09:31:27 +0100

    deploy

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/~ushift.daemon/apps/gtfshift/stage/',
+  base: '/~ushift.daemon/apps/gtfshift/',
   plugins: [svelte(), tailwindcss()],
   resolve: {
     alias: {
